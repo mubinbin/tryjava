8e49e647d5797d62bc01e053ebd9063c474ed2f3
mubinbin <dinobunmu@gmail.com>
2022-06-10 11:58:55 -0500

    commit rope tree

---
+++ src/RopeTree.java
@@ -0,0 +1,140 @@
+import java.util.List;
+
+import javax.imageio.plugins.tiff.TIFFDirectory;
+
+public class RopeTree {
+    Node root;
+
+    private class Node {
+        int length;
+        Node parent;
+
+        public Node(int length, Node parent) {
+            this.length = length;
+            this.parent = parent;
+        }
+    }
+
+    private class LeafNode extends Node {
+        String str;
+        
+        public LeafNode(int length, Node parent, String str) {
+            super(length, parent);
+            this.str = str;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Leaf(").append(this.length).append(", ").append(this.str).append(")");
+            return sb.toString();
+        }
+    }
+
+    private class InternalNode extends Node {
+        Node left;
+        Node right;
+
+        public InternalNode(int length, Node parent, Node left, Node right) {
+            super(length, parent);
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("internalNode, ").append(this.length);
+            return sb.toString();
+        }
+    }
+
+    public RopeTree() {
+        root = new InternalNode(0, null, null, null);
+    }
+
+    public Node add(String s) {
+        return add(root, null, s);
+    }
+
+    private Node add(Node root, InternalNode parent, String s) {
+        int len = s.length();
+        if (root == null) return new LeafNode(len, parent, s);
+
+        int rootLen = root.length;
+        if (len > rootLen) {
+            // go right; 2 cases, IN and LN
+            if (root instanceof InternalNode) {
+                root.length = len + rootLen;
+                InternalNode pointer = (InternalNode) root;
+                pointer.right = add(pointer.right, pointer, s);
+                return pointer;
+            }
+            else if (root instanceof LeafNode) {
+                LeafNode left = (LeafNode) root;
+                InternalNode newInternalNode = new InternalNode(len + rootLen, parent, left, null);
+                newInternalNode.right = add(newInternalNode.right, newInternalNode, s);
+                return newInternalNode;
+            }
+        }
+        else {
+            if (root instanceof InternalNode) {
+                root.length = len + rootLen;
+                InternalNode pointer = (InternalNode) root;
+                pointer.left = add(pointer.left, pointer, s);
+                return pointer;
+            }
+            else if (root instanceof LeafNode) {
+                LeafNode right = (LeafNode) root;
+                InternalNode newInternalNode = new InternalNode(len + rootLen, parent, null, right);
+                newInternalNode.left = add(newInternalNode.left, newInternalNode, s);
+                return newInternalNode;
+            }
+        }
+        return root;
+    }
+
+    public void print(Node root) {
+        if (root == null) {
+            System.out.println("null");
+            return;
+        }
+        if (root instanceof LeafNode) {
+            System.out.println(root.toString());
+            return;
+        }
+
+        if (root instanceof InternalNode) {
+            InternalNode pointer = (InternalNode) root;
+            System.out.println(root.toString());
+            print(pointer.left);
+            print(pointer.right);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        String input0 = "asfoinf lnwfoio weofnwiodsdf";
+        String input1 = "asfoinf lnwfoio weofnwiod";
+        String input2 = "woiejr pajeofdf;lmoeifpoj 999999999999999999999";
+        String input3 = "iojwoemf3poj-0sadfkm3  oi2rj-90fdjplnk32oirSK-M WEWEJ PWEJRLkanfdo";
+        String input4 = "iojwoemf3poj-0sadfkm3  oi2rj-90fdjplnk32oirSK-M WEWEJ PWEJRLkanfdoasfoinf lnwfoio weofnwiodsdfGGG";
+        
+        int len0 = input0.length();
+        int len1 = input1.length();
+        int len2 = input2.length();
+        int len3 = input3.length();
+        int len4 = input4.length();
+        System.out.println(List.of(len0, len1, len2, len3, len4));
+
+        RopeTree rt = new RopeTree();
+        rt.add(input0);
+        rt.add(input2);
+        rt.add(input3);
+        rt.add(input1);
+        rt.add(input4);
+        rt.print(rt.root);
+        
+    }
+}
---
+++ src/SortList.java
@@ -0,0 +1,74 @@
+public class SortList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        @Override
+        public String toString() {
+            return String.valueOf(val);
+        }
+
+    }
+
+    public static void print(ListNode node) {
+        if (node == null) return;
+
+        System.out.println(node.toString());
+        print(node.next);
+    }
+
+    public static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+
+    static ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    }
+
+    static ListNode getMid(ListNode head) {
+        ListNode midPrev = null;
+        while (head != null && head.next != null) {
+            midPrev = (midPrev == null) ? head : midPrev.next;
+            head = head.next.next;
+        }
+        ListNode mid = midPrev.next;
+        midPrev.next = null;
+        return mid;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode root = new ListNode(4);
+        ListNode n2 = new ListNode(2);
+        ListNode n1 = new ListNode(1);
+        ListNode n3 = new ListNode(3);
+        root.next = n2;
+        n2.next = n1;
+        n1.next = n3;
+
+        root = sortList(root);
+        print(root);
+    }
+}
